d4ca3c4845fbb03acab034318833145100090623
DarkVenom777 <lazarusvenom@gmail.com>
2020-07-27 14:38:10 +0000

    X00T:Bringup for bootleg

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -15,9 +15,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_X00T.mk
+    $(LOCAL_DIR)/bootleg_X00T.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_X00T-user \
-    lineage_X00T-userdebug \
-    lineage_X00T-eng
+    bootleg_X00T-user \
+    bootleg_X00T-userdebug \
+    boitleg_X00T-eng
